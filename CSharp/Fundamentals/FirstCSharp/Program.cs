92db1cdd9c7f800b92ff33dbb7610cd6dd5f4401
Alex Stack <alex_stack012@live.com>
2021-11-03 10:51:09 -0400

    Add files via upload

---
+++ CSharp/Fundamentals/FirstCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable to interpolate
+            string place = "Coding Dojo";
+            //Interpolated string, note the $
+            string message = $"Hello {place}";
+            // Displaying final message
+            Console.WriteLine(message);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
